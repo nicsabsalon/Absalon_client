81f20c6ae1fef7e584d36d7fdbd43aee10d53c6f
nicsabsalon <absalonnicole2@gmail.com>
2024-05-11 01:26:46 +0800

    initial commit

---
+++ componets/navigation.vue
@@ -0,0 +1,15 @@
+<template>
+    <nav class="bg-gray-800 p-4">
+      <div class="container mx-auto flex justify-between items-center">
+        <div class="text-white font-bold">Logo</div>
+        <div>
+          <NuxtLink to="/" class="text-white hover:text-gray-400 mx-2">Home</NuxtLink>
+          <NuxtLink to="/category" class="text-white hover:text-gray-400 mx-2">Category</NuxtLink>
+          <NuxtLink to="/product Features" class="text-white hover:text-gray-400 mx-2">Products Features</NuxtLink>
+          <NuxtLink to="/checkout" class="text-white hover:text-gray-400 mx-2">Checkout</NuxtLink>
+          <NuxtLink to="/Shoppingcart" class="text-white hover:text-gray-400 mx-2">Shopping Cart</NuxtLink>
+        </div>
+      </div>
+    </nav>
+</template>
+  
---
+++ pages/index.vue
@@ -0,0 +1,33 @@
+<!--
+  This example requires some changes to your config:
+  
+  ```
+  // tailwind.config.js
+  module.exports = {
+    // ...
+    plugins: [
+      // ...
+      require('@tailwindcss/forms'),
+      require('@tailwindcss/aspect-ratio'),
+    ],
+  }
+  ```
+-->
+<template>
+    <div>
+      <Navigation />
+      <NuxtLayout name="default">
+        <!-- Your page content goes here -->
+      </NuxtLayout>
+    </div>
+  </template>
+  
+  <script>
+  import Navigation from '~/components/navigation.vue'
+  
+  export default {
+    components: {
+      Navigation
+    }
+  }
+  </script>
